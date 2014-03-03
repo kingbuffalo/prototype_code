3cd8fabb8febbe5a7c69d48f0b9548f0c9220afd
kingbuffalo <kinghexi@126.com>
2014-03-03 16:01:39 +0800

    go_add

---
+++ go/basic/for.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+
+func main(){
+    i:=0
+    for i<3 {
+        fmt.Println(i)
+        i++
+    }
+
+    for i=0;i<10;i++{
+        fmt.Println(i)
+    }
+
+    for {
+        fmt.Println("loop")
+    }
+}
